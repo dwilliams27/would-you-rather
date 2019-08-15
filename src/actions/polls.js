08fee05b86f911a74a0f547ec3fd744534188faf
David Williams <davo49fb@gmail.com>
2019-08-14 23:27:32 -0400

    Not needed

--- src/actions/polls.js
+++ src/actions/polls.js
@@ -1,19 +1,11 @@
 export const RECEIVE_POLLS = 'RECEIVE_POLLS';
 export const TOGGLE_POLL_LIST = 'TOGGLE_POLL_LIST';
 export const POPULATE_POLL_LIST = 'POPULATE_POLL_LIST';
-export const ADD_QUESTION_POLE = 'ADD_QUESTION_POLE';
 
 export function receivePolls (polls) {
   return {
     type: RECEIVE_POLLS,
     polls
-  }
-}
-
-export function addQuestionPole (question) {
-  return {
-    type: ADD_QUESTION_POLE,
-    question
   }
 }
 
